1cfb5658097eca6b0ddc4ad1a119aa6c1b29b7b8
Matéo DELARBRE <mateo.delarbre@gmail.com>
2019-04-24 22:20:19 +0200

    debug process

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mdelarbr <mdelarbr@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/18 13:43:41 by mdelarbr     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/24 18:10:24 by mdelarbr    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/24 22:19:01 by mdelarbr    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,7 +14,7 @@
 #include "../../includes/exec.h"
 #include "../../includes/lexeur.h"
 
-int			cnt_res(t_lexeur **res, int i)
+int			cnt_process(t_lexeur **res, int i)
 {
 	int		nb;
 
@@ -27,16 +27,52 @@ int			cnt_res(t_lexeur **res, int i)
 	return (nb);
 }
 
+void		init_process(t_process *p)
+{
+	p->completed = '\0';
+	p->stopped = '\0';
+}
+
 void		fill_process(t_job *j, t_lexeur **res)
 {
-	int		i;
+	int			i;
+	int			k;
+	t_process	*start;
 
 	i = 0;
-	while (res[i] && (res[i]->token != 1 || res[i]->token != 8))
+	j->p = malloc(sizeof(t_process));
+	start = j->p;
+	init_process(j->p);
+	while (res[i])
+	{
+		k = 0;
+		j->p->cmd = malloc(sizeof(char *) * (cnt_process(res, i) + 1));
+		while (res[i] && res[i]->word)
+		{
+			j->p->cmd[k] = ft_strdup(res[i]->word);
+			i++;
+			k++;
+		}
+		j->p->cmd[k] = NULL;
+		if (res[i] && (res[i]->token != 1 && res[i]->token != 8))
+		{
+			j->p->next = malloc(sizeof(t_process));
+			j->p = j->p->next;
+			init_process(j->p);
+		}
+		else if (res[i])
+		{
+			j->p->next = NULL;
+			j->p = start;
+			j = j->next;
+			j->p = malloc(sizeof(t_process));
+		}
+		else
+			break ;
 		i++;
-	if (!res[i])
-		/*envoyer direct a execve*/
-	/*faire une foret de if avec tout les tokens*/
+	}
+	j->p->next = NULL;
+	j->p = start;
 }
 
 void		print_job(t_job *j)
@@ -44,25 +80,29 @@ void		print_job(t_job *j)
 	int		job;
 	int		i;
 	int		process;
+	t_process	*start;
 
 	job = 0;
 	i = 0;
+	start = j->p;
 	process = 0;
 	while (j)
 	{
-		printf("\nj[%d]->next: _%p_\tsplit: _%c_\n", job, j->next, j->split);
+		printf("\n---jobs---[%d]->next: _%p_\tsplit: _%c_\n", job, j->next, j->split);
 		job++;
-		/*while (j->p)
+		while (j->p)
 		{
-			printf("\np[%d]->next: _%p_\n", process, j->p->next);
+			printf("--process--[%d]->next: _%p_\n", process, j->p->next);
 			process++;
 			while (j->p->cmd[i])
 			{
-				printf("\ncmd[%d]-> _%s_\n", i, j->p->cmd[i]);
+				printf("cmd[%d]-> _%s_\n", i, j->p->cmd[i]);
 				i++;
 			}
+			i = 0;
 			j->p = j->p->next;
-		}*/
+		}
+		j->p = start;
 		process = 0;
 		j = j->next;
 	}
@@ -79,15 +119,16 @@ void		fill_job(t_job *j, t_lexeur **res)
 {
 	int			i;
 	int			k;
-	t_job		*start;
+//	t_job		*start;
 
-	start = j;
+//	start = *j;
 	i = 0;
 	k = 0;
 	while (res[i])
 	{
 		if (res[i]->token == 1 || res[i]->token == 8)
 		{
+			puts("\nlaaaaaaaa");
 			if (res[i]->token == 1)
 				j->split = '&';
 			else
@@ -100,7 +141,7 @@ void		fill_job(t_job *j, t_lexeur **res)
 			i++;
 	}
 	j->next = NULL;
-	j = start;
+//	j = start;
 }
 
 int			start_exec(t_lexeur **res)
